2b0c9f5092e7e7047b76bbb78341437e84042b70
Neil Coggins <ncoggins@jupiter.localdomain>
2021-07-05 11:56:21 +0100

    fixed log file path; updated copyright message

--- src/blockchain_utilities/blockchain_scanner.cpp
+++ src/blockchain_utilities/blockchain_scanner.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2014-2019, The Monero Project
+// Copyright (c) 2021, Haven Protocol
+// Portions copyright (c) 2014-2019, The Monero Project
 //
 // All rights reserved.
 //
@@ -100,7 +101,7 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  mlog_configure(mlog_get_default_log_path("monero-blockchain-stats.log"), true);
+  mlog_configure(mlog_get_default_log_path("haven-blockchain-scanner.log"), true);
   if (!command_line::is_arg_defaulted(vm, arg_log_level))
     mlog_set_log(command_line::get_arg(vm, arg_log_level).c_str());
   else
