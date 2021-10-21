aa7e2dd37c00ab5d56859b2f7cb6ece67dfc45ef
akildemir <aeg_asd@hotmail.com>
2021-10-21 11:01:54 +0200

    set stagenet hf height; bumb version

--- src/hardforks/hardforks.cpp
+++ src/hardforks/hardforks.cpp
@@ -78,7 +78,8 @@ const hardfork_t stagenet_hard_forks[] = {
   { 14, 70, 0, 1593616242 },
   { 15, 150, 0, 1593616243 },
   { 16, 230, 0, 1593616244 },
-  { 17, 15000, 0, 1593616244 }
+  { 17, 31000, 0, 1597456244 },
+  { 18, 31600, 0, 1597498244 }
 };
 const size_t num_stagenet_hard_forks = sizeof(stagenet_hard_forks) / sizeof(stagenet_hard_forks[0]);
 const uint64_t stagenet_hard_fork_version_1_till = 24;
