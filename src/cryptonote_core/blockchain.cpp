c385817e19413f503e75047c1e04e89eebdd5d85
Neil Coggins <ncoggins@jupiter.localdomain>
2022-01-26 09:55:32 +0000

    fixed miner TX validation on sync; bumped version number

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -1709,8 +1709,17 @@ bool Blockchain::prevalidate_miner_transaction(const block& b, uint64_t height, 
     return false;
   }
   MDEBUG("Miner tx hash: " << get_transaction_hash(b.miner_tx));
-  CHECK_AND_ASSERT_MES(b.miner_tx.unlock_time == height + CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW, false, "coinbase transaction transaction has the wrong unlock time=" << b.miner_tx.unlock_time << ", expected " << height + CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW);
-
+  if ((hf_version >= HF_PER_OUTPUT_UNLOCK_VERSION) && (b.miner_tx.version >= POU_TRANSACTION_VERSION)) {
+    // HF19, TXv6+ - use per_output unlock times vector
+    CHECK_AND_ASSERT_MES(b.miner_tx.output_unlock_times.size() >= 2, false, "coinbase transaction in the block has too few unlock times");
+    CHECK_AND_ASSERT_MES(b.miner_tx.output_unlock_times.size() % 2 == 0, false, "coinbase transaction in the block has incorrect count of unlock times (" << b.miner_tx.output_unlock_times.size() << ")");
+    for (const auto pou: b.miner_tx.output_unlock_times) {
+      CHECK_AND_ASSERT_MES(pou == height + CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW, false, "coinbase transaction transaction has the wrong unlock time=" << pou << ", expected " << height + CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW);
+    }
+  } else {
+    CHECK_AND_ASSERT_MES(b.miner_tx.unlock_time == height + CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW, false, "coinbase transaction transaction has the wrong unlock time=" << b.miner_tx.unlock_time << ", expected " << height + CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW);
+  }
+  
   //check outs overflow
   //NOTE: not entirely sure this is necessary, given that this function is
   //      designed simply to make sure the total amount for a transaction
