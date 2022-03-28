3aeebc7d571ff75527d51ebad8d5043f16f216ad
Neil Coggins <ncoggins@jupiter.localdomain>
2022-03-28 09:15:20 +0100

    clamping of burnt asset tally to zero to reduce error in supply figures due to 2021 hack

--- src/blockchain_db/lmdb/db_lmdb.cpp
+++ src/blockchain_db/lmdb/db_lmdb.cpp
@@ -1093,6 +1093,12 @@ uint64_t BlockchainLMDB::add_transaction_data(const crypto::hash& blk_hash, cons
     MDB_val_copy<uint64_t> source_idx(cs.source_currency_type);
     boost::multiprecision::int128_t source_tally = read_circulating_supply_data(m_cur_circ_supply_tally, source_idx);
     boost::multiprecision::int128_t final_source_tally = source_tally - cs.amount_burnt;
+    boost::multiprecision::int128_t coinbase = get_block_already_generated_coins(m_height-1);
+    if ((strSource == "XHV" && (coinbase + final_source_tally < 0)) ||
+        (strSource != "XHV" && final_source_tally < 0)) {
+      LOG_ERROR(__func__ << " : mint/burn underflow detected for " << strSource << " : correcting supply tally by " << final_source_tally);
+      final_source_tally = 0;
+    }
     write_circulating_supply_data(m_cur_circ_supply_tally, source_idx, final_source_tally);
 
     // Get the current tally value for the dest currency type
@@ -1112,6 +1118,7 @@ uint64_t BlockchainLMDB::add_transaction_data(const crypto::hash& blk_hash, cons
 // passing it in to functions like this
 void BlockchainLMDB::remove_transaction_data(const crypto::hash& tx_hash, const transaction& tx, bool miner_tx)
 {
+  uint64_t m_height = height();
   int result;
 
   LOG_PRINT_L3("BlockchainLMDB::" << __func__);
@@ -1198,6 +1205,12 @@ void BlockchainLMDB::remove_transaction_data(const crypto::hash& tx_hash, const 
     MDB_val_copy<uint64_t> dest_idx(cs.dest_currency_type);
     boost::multiprecision::int128_t dest_tally = read_circulating_supply_data(m_cur_circ_supply_tally, dest_idx);
     boost::multiprecision::int128_t final_dest_tally = dest_tally - cs.amount_minted;
+    boost::multiprecision::int128_t coinbase = get_block_already_generated_coins(m_height-1);
+    if ((strDest == "XHV" && (coinbase + final_dest_tally < 0)) ||
+        (strDest != "XHV" && final_dest_tally < 0)) {
+      LOG_ERROR(__func__ << " : mint/burn underflow detected for " << strDest << " : correcting supply tally by " << final_dest_tally);
+      final_dest_tally = 0;
+    }
     write_circulating_supply_data(m_cur_circ_supply_tally, dest_idx, final_dest_tally);
 
     // Update the circ_supply table
