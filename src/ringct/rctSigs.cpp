684cb56497bf142f07e8c7c6110330c7c86024d1
akildemir <aeg_asd@hotmail.com>
2022-11-22 11:15:37 +0300

    fix indentation

--- src/ringct/rctSigs.cpp
+++ src/ringct/rctSigs.cpp
@@ -1556,16 +1556,16 @@ namespace rct {
       if (strSource != strDest) {
 
         if ((version < HF_VERSION_USE_COLLATERAL) && (tx_type == tt::XASSET_TO_XUSD || tx_type == tt::XUSD_TO_XASSET)) {
-	  // Wallets must append the burnt fee for xAsset conversions to the amount_burnt.
-	  // So we subtract that from amount_burnt and validate only the actual coversion amount because
-	  // fees are not converted. They are just burned.
+          // Wallets must append the burnt fee for xAsset conversions to the amount_burnt.
+          // So we subtract that from amount_burnt and validate only the actual coversion amount because
+          // fees are not converted. They are just burned.
 
-	  // calculate the burnt fee. Should be the 80% of the offshoreFee
-	  boost::multiprecision::uint128_t fee_128 = rv.txnOffshoreFee;
-	  boost::multiprecision::uint128_t burnt_fee = (fee_128 * 4) / 5;
-              
-	  // subtract it from amount burnt
-	  amount_burnt -= (uint64_t)burnt_fee;
+          // calculate the burnt fee. Should be the 80% of the offshoreFee
+          boost::multiprecision::uint128_t fee_128 = rv.txnOffshoreFee;
+          boost::multiprecision::uint128_t burnt_fee = (fee_128 * 4) / 5;
+
+          // subtract it from amount burnt
+          amount_burnt -= (uint64_t)burnt_fee;
         }
 
         // m = sum of all masks of inputs
@@ -1596,20 +1596,20 @@ namespace rct {
       // validate the collateral
       if ((version >= HF_VERSION_USE_COLLATERAL) && (tx_type == tt::OFFSHORE || tx_type == tt::ONSHORE)) {
 
-	// Retrieve collateral commitment that was provided by wallet
-	key C_col = rv.outPk[col_index].mask;
+        // Retrieve collateral commitment that was provided by wallet
+        key C_col = rv.outPk[col_index].mask;
 
-	// Generate commitment using stored mask and required amount of collateral
-	key pseudoC_col;
-	genC(pseudoC_col, rv.maskSums[2], amount_collateral);
+        // Generate commitment using stored mask and required amount of collateral
+        key pseudoC_col;
+        genC(pseudoC_col, rv.maskSums[2], amount_collateral);
 
-	// Verify that correct amount of collateral was provided
-	if (!equalKeys(pseudoC_col, C_col)) {
-	  LOG_ERROR("Offshore collateral verification failed.");
-	  return false;
+        // Verify that correct amount of collateral was provided
+        if (!equalKeys(pseudoC_col, C_col)) {
+          LOG_ERROR("Offshore collateral verification failed.");
+          return false;
         }
       }
-        
+
       for (size_t i = 0; i < rv.p.bulletproofs.size(); i++)
         proofs.push_back(&rv.p.bulletproofs[i]);
     
