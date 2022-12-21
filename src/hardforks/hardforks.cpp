ac5d46d87ee6ffe5de0f12d8a72b4c8550ba8114
akildemir <aeg_asd@hotmail.com>
2022-12-21 14:06:20 +0300

    set mainnet fork height

--- src/hardforks/hardforks.cpp
+++ src/hardforks/hardforks.cpp
@@ -39,13 +39,14 @@ const hardfork_t mainnet_hard_forks[] = {
   { 4, 290587, 0, 1553112000 },  // 20th March 2019 ~20:00 GMT
   { 5, 356338, 0, 1561060800 },  // 20th June 2019 - 20:00 GMT
   { 11, 640640, 0, 1595253600 },  // 20th July 2020 - 15:00 GMT
-  { 13, 640650, 0, 1595254800 },   // 20th July 2020 - 15:20 GMT
-  { 14, 653565, 0, 1596555180 },   // 7th August 2020 - 15:30 GMT
-  { 15, 741957, 0, 1606217525 },   // Fork time is on or around 8th December 2020 at 14:30 GMT. Fork time finalised on 2020-11-24.
-  { 16, 831700, 0, 1616597544 },   // Fork time is on or around 12th April 2021 at 12:50 GMT. Fork time finalised on 2021-03-24.
-  { 17, 886575, 0, 1624833973 },   // Fork time is on or around 27th June 2021 at 22:26 GMT. Fork time finalised on 2021-07-12. Yes, this is a fork set in the past, because of a chain rollback.
-  { 18, 973400, 0, 1636625720 },   // Fork time is on or around 18th November 2021 at 10:30 GMT. Fork time finalised on 2021-11-11.
-  { 19, 1033025, 0, 1643968173 }   // Fork time is on or around 9th February 2022 at 11:00 GMT. Fork time finalised on 2022-02-04.
+  { 13, 640650, 0, 1595254800 },  // 20th July 2020 - 15:20 GMT
+  { 14, 653565, 0, 1596555180 },  // 7th August 2020 - 15:30 GMT
+  { 15, 741957, 0, 1606217525 },  // Fork time is on or around 8th December 2020 at 14:30 GMT. Fork time finalised on 2020-11-24.
+  { 16, 831700, 0, 1616597544 },  // Fork time is on or around 12th April 2021 at 12:50 GMT. Fork time finalised on 2021-03-24.
+  { 17, 886575, 0, 1624833973 },  // Fork time is on or around 27th June 2021 at 22:26 GMT. Fork time finalised on 2021-07-12. Yes, this is a fork set in the past, because of a chain rollback.
+  { 18, 973400, 0, 1636625720 },  // Fork time is on or around 18th November 2021 at 10:30 GMT. Fork time finalised on 2021-11-11.
+  { 19, 1033025, 0, 1643968173 }, // Fork time is on or around 9th February 2022 at 11:00 GMT. Fork time finalised on 2022-02-04.
+  { 20, 1272875, 0, 1671618321 }  // Fork time is on or around 9th January 2023 at 10:00 GMT. Fork time finalised on 2022-12-21.
  };
 const size_t num_mainnet_hard_forks = sizeof(mainnet_hard_forks) / sizeof(mainnet_hard_forks[0]);
 const uint64_t mainnet_hard_fork_version_1_till = 38499;
