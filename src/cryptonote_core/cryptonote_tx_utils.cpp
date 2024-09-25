f2e55ce8d049678f8c975ff5f1a66dd06c2d4bbe
Tay8NWWFKpz9JT4NXU0w <user.email>
2024-09-25 23:38:23 +0000

    supply recalculation, miner reward fix

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -356,7 +356,6 @@ namespace cryptonote
           // Miner component of the xAsset TX fee
           r = crypto::derive_public_key(derivation, idx, miner_address.m_spend_public_key, out_eph_public_key);
           CHECK_AND_ASSERT_MES(r, false, "while creating outs: failed to derive_public_key(" << derivation << ", " << idx << ", "<< miner_address.m_spend_public_key << ")");
-          idx++;
           crypto::view_tag view_tag;
 
           if (hard_fork_version >= HF_VERSION_VIEW_TAGS) {
@@ -386,6 +385,7 @@ namespace cryptonote
             out_miner.target = tk_miner;
             tx.vout.push_back(out_miner);
           }
+          idx++;
 
           crypto::public_key out_eph_public_key_xasset = AUTO_VAL_INIT(out_eph_public_key_xasset);
           if (!get_deterministic_output_key(governance_wallet_address.address, gov_key, idx /* n'th output in miner tx */, out_eph_public_key_xasset, view_tag))
